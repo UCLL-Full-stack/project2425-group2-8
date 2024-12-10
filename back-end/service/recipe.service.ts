fa3be20b3c35595910ee4656af1a155d87b58f2b
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-12-10 11:53:54 +0100

    feat: add a new route to fetch ingredients from multiple recipes, and implement a service method to fetch and consolidate ingredients

--- back-end/service/recipe.service.ts
+++ back-end/service/recipe.service.ts
@@ -1,4 +1,6 @@
 import { Recipe } from '../model/recipe';
+import { RecipeIngredient } from '../model/recipeIngredient';
+import { Ingredient } from '../model/ingredient';
 import recipeDb from '../repository/recipe.db';
 import { RecipeUpdateInput } from '../types';
 
@@ -10,6 +12,47 @@ const getRecipeById = async (id: number): Promise<Recipe> => {
     const recipe = await recipeDb.getRecipeById({ id });
     if (!recipe) throw new Error(`Recipe with id ${id} does not exist.`);
     return recipe;
+};
+
+const getIngredientsForRecipes = async (recipeIds: number[]): Promise<any[]> => {
+    const ingredientsMap: Record<string, any> = {};
+
+    for (const recipeId of recipeIds) {
+        const recipe: Recipe | null = await recipeDb.getRecipeById({ id: recipeId });
+        if (recipe) {
+            const recipeIngredients: RecipeIngredient[] | undefined = recipe.getIngredients();
+            if (recipeIngredients) {
+                for (const recipeIngredient of recipeIngredients) {
+                    const ingredient: Ingredient | undefined = recipeIngredient.getIngredient();
+                    if (ingredient) {
+                        const key: string = `${ingredient.getName()}-${ingredient.getCategory()}`;
+                        if (ingredientsMap[key]) {
+                            ingredientsMap[key].quantity += recipeIngredient.getQuantity();
+                            ingredientsMap[key].recipes.push({
+                                recipeId: recipe.getId(),
+                                recipeTitle: recipe.getTitle(),
+                                quantity: recipeIngredient.getQuantity(),
+                            });
+                        } else {
+                            ingredientsMap[key] = {
+                                ...ingredient,
+                                quantity: recipeIngredient.getQuantity(),
+                                recipes: [
+                                    {
+                                        recipeId: recipe.getId(),
+                                        recipeTitle: recipe.getTitle(),
+                                        quantity: recipeIngredient.getQuantity(),
+                                    },
+                                ],
+                            };
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return Object.values(ingredientsMap);
 };
 
 const updateRecipe = async (
@@ -37,4 +80,10 @@ const deleteRecipe = async (id: number): Promise<void> => {
     await recipeDb.deleteRecipe({ id });
 };
 
-export default { getAllRecipes, getRecipeById, updateRecipe, deleteRecipe };
+export default {
+    getAllRecipes,
+    getRecipeById,
+    updateRecipe,
+    deleteRecipe,
+    getIngredientsForRecipes,
+};
